bd0fce71e3f9b7bfa65b320e22c0c51a9b3f6b3c
k.goto <ee57066@meiji.ac.jp>
2019-01-12 11:29:51 +0900

    必要なファイル作成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="main-bg">
+    <Header/>
+    <main>
+      <Message/>
+      <Profile/>
+      <Trend/>
+      <Portfolio/>
+      <Contact/>
+    </main>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
-
+import Header from './components/Header'
+import Message from './components/Message'
+import Profile from './components/Profile'
+import Trend from './components/Trend'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Message,
+    Profile,
+    Trend,
+    Portfolio,
+    Contact,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
+#main-bg {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
